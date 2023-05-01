0f9b9c64060bf12085739e885de5b6a2298dd9a1
Tolu Akinrinola <remmakins@gmail.com>
2023-05-01 03:48:17 -0400

    complete lab

--- main.js
+++ main.js
@@ -4,6 +4,66 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const articleHearts = document.querySelectorAll(".like-glyph");
+
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(){
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
+
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+
+
+
+/// MORE ADVANCED SOLUTION ///
+
+// const glyphStates = {
+//   "♡": "♥",
+//   "♥": "♡"
+// };
+
+// const colorStates = {
+//   "red" : "",
+//   "": "red"
+// };
+
+// const articleHearts = document.querySelectorAll(".like-glyph");
+
+// function likeCallback(e) {
+//   const heart = e.target;
+//   mimicServerCall("bogusUrl", {forceFailure: true})
+//     .then(function(){
+//        heart.innerText = glyphStates[heart.innerText];
+//        heart.style.color = colorStates[heart.style.color];
+//     })
+//     .catch(function(error) {
+//       const modal = document.getElementById("modal");
+//       modal.className = "";
+//       modal.innerText = error;
+//       setTimeout(() =>  modal.className = "hidden", 3000);
+//     });
+// }
+
+// for (const glyph of articleHearts) {
+//   glyph.addEventListener("click", likeCallback);
+// }
 
 
 
